04fbff20caf0e7d2bb9b713f28763a93c43816d7
Francesco <francesco.cocciro@gmail.com>
2023-06-23 17:55:28 +0200

    - Add two new pages to the interfaces

--- app/interface.py
+++ app/interface.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-app.static_folder = 'static'
 
 
 @app.route('/', methods=['GET'])
-def get_app():
-    css_url = url_for('static', filename='style.css')
-    return render_template('index.html', css_url=css_url)
+def index():
+    return render_template('./index.html')
 
 
-@app.route('/', methods=['POST'])
-def move_to_model_1():
-    pass
+@app.route('/model_1.html')
+def model_1():
+    return render_template('model_1.html')
+
+
+@app.route('/model_2.html')
+def model_2():
+    return render_template('model_2.html')
 
 
 @app.route('/', methods=['POST'])
